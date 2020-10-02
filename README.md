7e680caf65ec9ec1aa5ff5f5a3e8cd2bfbb89748
Lasith-Niro <lasithniro.online@gmail.com>
2020-10-02 10:02:22 +0100

    quadkey decoding added

--- README.md
+++ README.md
@@ -5,12 +5,17 @@
 - -c : coordinates of the image corners [min Longitude , min Latitude , max Longitude , max Latitude]
 
 ### Output
-geo-referenced tiff file
+- geo-referenced tiff file
 
 ### Files
 - img2geo.py : python program
 - utils.py   : helper functions
 - run.bat    : Windows .bat file that can be used to same functionality 
+
+### How to run
+```bash
+python .\img2geo.py -i .\result_z19.jpeg  -o test -c 53.3497,-6.2873,53.3584,-6.2741
+```
 ___
 
 > Manual geo-referencing
--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import math
 from osgeo import gdal
 from PIL import Image
+from pyproj import Transformer
+import mercantile
+
 
 EarthRadius = 6378137;
 MinLatitude = -85.05112878;
@@ -35,7 +38,6 @@ def parseToPoints(ip):
 	Y2 = max(y1,y2)
 	return Point(X1,Y1),Point(X1,Y2),Point(X2,Y1),Point(X2,Y2)
 
-from pyproj import Transformer
 def projection_3857(point):
 	transformer = Transformer.from_crs("epsg:4326", "epsg:3857")
 	x, y =  transformer.transform( point.getLat(), point.getLon())
@@ -50,3 +52,11 @@ def toGCP(coordinates, width, height, elevation):
 		gcp.append(gdal.GCP(lon, lat, elevation, arr2[i][0], arr2[i][1]))
 		# print("ground_coord_points.append(gdal.GCP({},{},0,{},{}))".format(lon, lat, arr2[i][0],arr2[i][1]))
 	return gcp
+
+def quadKeyToip(qyadkey):
+	tile = mercantile.quadkey_to_tile(qyadkey)
+	[left,bottom,right,top] = mercantile.bounds(tile.x, tile.y, tile.z)
+	return [left,bottom,right,top]
+
+# bbox = quadKeyToip("0313103132223020121")
+# print(",".join([str(i) for i in bbox]))
